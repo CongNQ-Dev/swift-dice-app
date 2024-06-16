19fc6bf5669834d5e04a8702aea9688628d6438a
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-06-16 14:41:04 +0700

    feat: action button

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageViewOne?.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
         diceImageViewOne.alpha = 0.5
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFive")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFive")
+    }
 }
+
+
 
