f8da309fb8af3fc34b4b173db151fe98794203e3
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-06-16 16:34:51 +0700

    feat: variables and arrays

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,27 +1,24 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    var leftSide=1
+    var rightSide=5
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
-        diceImageViewOne.alpha = 0.5
+      
     }
     
-    @IBAction func rollButtonPressed(_ sender: Any) {
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFive")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFive")
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("hihi")
+        diceImageViewOne.image=[#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftSide]
+        diceImageViewTwo.image=[#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightSide]
+        leftSide+=1
+        rightSide-=1
     }
 }
 
